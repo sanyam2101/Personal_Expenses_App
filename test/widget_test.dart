ca0f26f634b510932df9453e9cf7a2edef38b832
sanyam2101 <108520572+sanyam2101@users.noreply.github.com>
2022-07-30 10:26:03 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Personal_Expenses_App/main.dart';
+import 'package:personal_expenses/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
